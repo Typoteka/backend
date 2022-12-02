8fb4b747f0f2966e6504e89c2a40bc5a02cfc200
Nikita Maslov <maslov.n.e@gmail.com>
2022-12-02 10:49:53 +0200

    change article image path

--- app/core/models.py
+++ app/core/models.py
@@ -18,7 +18,7 @@ def image_file_path(instance, filename):
     ext = filename.split('.')[-1]
     filename = f'{uuid.uuid4()}.{ext}'
 
-    return os.path.join('uploads', 'recipe', filename)
+    return os.path.join('uploads', 'article', filename)
 
 
 class UserManager(BaseUserManager):
